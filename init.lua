db95aed841a30ad565d9b72eef41650c22383b0b
Jabbers99 <Joeylbyrne@gmail.com>
2019-07-06 10:49:47 +0000

    repairing discordia submodule

--- init.lua
+++ init.lua
@@ -1,16 +1,11 @@
 local discordia = require('discordia')
 local client = discordia.Client()
 
-local settings = require('./settings.lua')
 
 client:on('ready', function()
 	print('Logged in as '.. client.user.username)
 end)
 
-client:on('messageCreate', function(message)
-	if message.content == '!ping' then
-		message.channel:send('Pong!')
-	end
-end)
+print("test")
 
 client:run('Bot '..settings.token)
