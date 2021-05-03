297155d42a67d1eb942d20591db77f8b1124cef4
Jabbers99 <39739543+Jabbers99@users.noreply.github.com>
2021-05-03 22:04:38 +0100

    remove comment

--- commands/mute.lua
+++ commands/mute.lua
@@ -6,7 +6,7 @@ AddCommand("mute", function(msg, args)
 	local membersToMute = _G.Daemen.Functions.FindMentions(args)
 	for _, m in pairs(membersToMute) do
 		local mem = msg.guild:getMember(m)
-		--mem:AddRole(mutedRole)
+		mem:AddRole(mutedRole)
 		msg.channel:sendf("Muting member `%s`", _G.Daemen.Functions.MakeSafe(mem.tag))
 
 	end
