38b818c1bcd9574eea369f8b1b678dece33bef80
Jabbers99 <39739543+Jabbers99@users.noreply.github.com>
2021-05-04 19:36:17 +0100

    Add confirmation message to reminder

--- commands/remind.lua
+++ commands/remind.lua
@@ -16,13 +16,15 @@ AddCommand("remind", function(msg, args)
 	-- Set the reminder
 	delay = delay*multipliers[unit]
 	local chnl = msg.channel
+	local mentionString = msg.author.mentionString
 	if not _G.Daemen.Reminders[msg.author.id] or _G.Daemen.Reminders[msg.author.id] < 3 then
 		_G.Daemen.Reminders[msg.author.id] = 0
 		Timer.setTimeout(delay, function()
-			coroutine.wrap(function() chnl:sendf("%s %s milliseconds ago, you asked me to remind you about `%s`", msg.author.mentionString, delay, reminder) end)()
+			coroutine.wrap(function() chnl:sendf("%s %s milliseconds ago, you asked me to remind you about `%s`", msg.author.mentionString, tostring(math.floor(delay/60000)).."m", reminder) end)()
 			_G.Daemen.Reminders[msg.author.id] = _G.Daemen.Reminders[msg.author.id] + 1
 			if _G.Daemen.Reminders[msg.author.id] >= 3 then _G.Daemen.Reminders[msg.author.id] = nil end
 		end)
+		chnl:sendf("%s, in %s: `%s`, msg.mentionString, delay, reminder)
 	else
 		msg:reply("You already have three set reminders: "..reminder)
 	end
